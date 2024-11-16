014e6f5fe5e6fc59c75c903a2ef6059304be2afa
zaenramadani122 <fandozkizen@gmail.com>
2024-11-16 15:07:58 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,6 @@
   <title>Document</title>
 </head>
 <body>
-  <h1>nyunuk</h1>
+  <h1>meluncur</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('welcome');
-});
-
 Route::get('/masuk', function () {
-    return view('home');
+    return view('welcome');
 });
